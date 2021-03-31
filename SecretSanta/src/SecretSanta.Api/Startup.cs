580aceb085c020afef8d9ad304d87779d9f689db
Justin Plett <jmplett@me.com>
2021-03-30 17:05:31 -0700

    Assignment 1.2
    Changed the text in the API

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -32,7 +32,7 @@ namespace SecretSanta.Api
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello from API!");
+                    await context.Response.WriteAsync("Hello from world API! The second part of this assignment is done!!!");
                 });
             });
         }
