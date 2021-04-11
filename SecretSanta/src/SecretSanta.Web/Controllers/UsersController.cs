bc4aad98cb591baee00fb1609cf222547f30d1ed
Justin Plett <jmplett@me.com>
2021-04-11 15:03:22 -0700

    Basic Set Up Complete
    Basic set up done and a little CRUD with users but not yet complete

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SecretSanta.Web.ViewModels;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        static List<UserViewModel> Users = new List<UserViewModel>
+        {
+            new UserViewModel {FirstName = "Justin", LastName = "Plett"},
+            new UserViewModel {FirstName = "Jimmy", LastName = "Jo"},
+        };
+
+        public IActionResult Index()
+        {
+            return View(Users);
+        }
+    }
+}
