7bbd07f7423f923e91bcf01d53fcd95b0ecfee11
Justin Plett <jmplett@me.com>
2021-04-11 16:33:22 -0700

    Group CRUD Done

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SecretSanta.Web.ViewModels;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GroupsController : Controller
+    { 
+
+        static List<GroupViewModel> Groups = new List<GroupViewModel>
+        {
+            new GroupViewModel {GroupName = "Group1"},
+            new GroupViewModel {GroupName = "Group2"},
+        };
+
+        public IActionResult Index()
+        {
+            return View(Groups);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(GroupViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Groups.Add(viewModel);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Groups[id].Id = id;
+            return View(Groups[id]);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(GroupViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Groups[viewModel.Id] = viewModel;
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(int id)
+        {
+            Groups.RemoveAt(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GroupViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecretSanta.Web.ViewModels
+{
+    public class GroupViewModel
+    {
+        public int Id { get; set;}
+        [Required]
+        [Display(Name ="Group Name")]
+        public string GroupName { get; set; } = "";
+
+    }
+}
