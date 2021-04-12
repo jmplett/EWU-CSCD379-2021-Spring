60b968d690595b69ae5146ea9c7ae924f6a92971
Justin Plett <jmplett@me.com>
2021-04-11 17:15:39 -0700

    Gift Basic CRUD Done
    Need to still implement user part.

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GiftsController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SecretSanta.Web.ViewModels;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GiftsController : Controller
+    { 
+
+        static List<GiftViewModel> Gifts = new List<GiftViewModel>
+        {
+            new GiftViewModel {GiftName = "Gift1", GiftDescription = "It's a gift", GiftURL="www.Amazon.com", GiftPriority=1, GiftUser="Justin"},
+        };
+
+        public IActionResult Index()
+        {
+            return View(Gifts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(GiftViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Gifts.Add(viewModel);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Gifts[id].Id = id;
+            return View(Gifts[id]);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(GiftViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Gifts[viewModel.Id] = viewModel;
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+
+        public IActionResult Delete(int id)
+        {
+            Gifts.RemoveAt(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecretSanta.Web.ViewModels
+{
+    public class GiftViewModel
+    {
+        public int Id { get; set;}
+
+        [Required]
+        [Display(Name ="Name")]
+        public string GiftName { get; set; } = "";
+
+        [Required]
+        [Display(Name = "Description")]
+        public string GiftDescription { get; set; } = "";
+
+        [Required]
+        [Display(Name = "URL")]
+        public string GiftURL { get; set; } = "";
+
+        [Required]
+        [Display(Name = "Priority")]
+        public int GiftPriority { get; set; }
+
+
+        [Required]
+        [Display(Name = "User")]
+        public string GiftUser { get; set; } = "";
+
+    }
+}
