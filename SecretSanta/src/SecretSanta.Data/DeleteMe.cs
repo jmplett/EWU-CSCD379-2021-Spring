481221ffced01bb4c593501bdd55e937a59a03e0
Justin Plett <jmplett@me.com>
2021-04-25 19:37:18 -0700

    Temp Delete ME

---
+++ SecretSanta/src/SecretSanta.Data/DeleteMe.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace SecretSanta.Data
+{
+    public static class DeleteMe
+    {
+        public static List<string> Users { get; } = new()
+        {
+            "Justin Plett",
+            "Tim Tracker",
+            "Poly Paul"
+        };
+    }
+}
