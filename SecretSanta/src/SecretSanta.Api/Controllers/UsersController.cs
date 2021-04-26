a752d6f3605cada7f9881338c50200d62d124625
Justin Plett <jmplett@me.com>
2021-04-26 12:54:26 -0700

    didn't add a couple files

--- SecretSanta/src/SecretSanta.Api/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Api/Controllers/UsersController.cs
@@ -1,45 +1,78 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Business;
+using SecretSanta.Data;
 
-namespace SecretSanta.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-        // /api/events
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return DeleteMe.Users;
-        }
+namespace SecretSanta.Api.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase {
+    private IUserRepository UserManager { get; }
 
-        // /api/events/<index>
-        [HttpGet("{index}")]
-        public string Get(int index)
-        {
-            return DeleteMe.Users[index];
-        }
-
-        //DELETE /api/events/<index>
-        [HttpDelete("{index}")]
-        public void Delete(int index)
-        {
-            DeleteMe.Users.RemoveAt(index);
-        }
-
-        // POST /api/events
-        [HttpPost]
-        public void Post([FromBody] string eventName)
-        {
-            DeleteMe.Users.Add(eventName);
-        }
-
-        // PUT /api/events
-        [HttpPut("{index}")]
-        public void Put(int index, [FromBody]string userName)
-        {
-            DeleteMe.Users[index] = userName;
-        }
+    public UsersController(IUserRepository userManager) {
+      UserManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
     }
+
+    [HttpGet]
+    public IEnumerable<User> Get() {
+      return UserManager.List();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<User?> Get(int id) {
+      if (id < 0) {
+        return NotFound();
+      }
+
+      User? returnedUser = UserManager.GetItem(id);
+      return returnedUser;
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id) {
+      if (id < 0) {
+        return NotFound();
+      }
+
+      if (UserManager.Remove(id)) {
+        return Ok();
+      }
+
+      return NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<User?> Post([FromBody] User? myUser) {
+      if (myUser is null) {
+        return BadRequest();
+      }
+
+      return UserManager.Create(myUser);
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody] User? myUser) {
+      if (myUser is null) {
+        return BadRequest();
+      }
+
+      User? foundUser = UserManager.GetItem(id);
+
+      if (foundUser is not null) {
+        if (!string.IsNullOrWhiteSpace(myUser.FirstName)) {
+          foundUser.FirstName = myUser.FirstName;
+        }
+
+        if (!string.IsNullOrWhiteSpace(myUser.LastName)) {
+          foundUser.LastName = myUser.LastName;
+        }
+
+        UserManager.Save(foundUser);
+        return Ok();
+      }
+
+      return NotFound();
+    }
+  }
 }
