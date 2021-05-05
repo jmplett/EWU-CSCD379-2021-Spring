328390604a583623b0fadeb43433ebf0355d572e
Justin Plett <jmplett@me.com>
2021-05-04 21:08:15 -0700

    Fixing of deleted commits from computer back up

---
+++ SecretSanta/src/SecretSanta.Api/Controllers/UsersController 2.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Business;
+using SecretSanta.Data;
+using SecretSanta.Api.Dto;
+
+namespace SecretSanta.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserRepository Repository { get; }
+
+        public UsersController(IUserRepository repository)
+        {
+            Repository = repository ?? throw new System.ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IEnumerable<User> Get()
+        {
+            return Repository.List();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        public ActionResult<User?> Get(int id)
+        {
+            User? user = Repository.GetItem(id);
+            if (user is null) return NotFound();
+            return user;
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult Delete(int id)
+        {
+            if (Repository.Remove(id))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        public ActionResult<User?> Post([FromBody] User? user)
+        {
+            if (user is null)
+            {
+                return BadRequest();
+            }
+            return Repository.Create(user);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Put(int id, [FromBody]UpdateUser? updatedUser)
+        {
+            if (updatedUser is null)
+            {
+                return BadRequest();
+            }
+
+            User? foundUser = Repository.GetItem(id);
+            if (foundUser is not null)
+            {
+                if(!string.IsNullOrWhiteSpace(updatedUser.FirstName))
+                {
+                    foundUser.FirstName = updatedUser.FirstName ?? "";
+                }
+                foundUser.LastName = updatedUser.LastName ?? "";
+
+                Repository.Save(foundUser);
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
