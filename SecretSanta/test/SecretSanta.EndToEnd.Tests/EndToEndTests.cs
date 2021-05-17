0595258462c0048a2266946842364e7f795e2231
Justin Plett <jmplett@me.com>
2021-05-16 21:22:47 -0700

    Setup of Playwrite and end to end tests

---
+++ SecretSanta/test/SecretSanta.EndToEnd.Tests/EndToEndTests.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlaywrightSharp;
+
+namespace SecretSanta.EndToEnd.Tests
+{
+    [TestClass]
+    public class EndToEndTests
+    {
+       
+}
