4c4ce68c6fea70a0ff4f2526a8a64ebc6b72472d
Justin Plett <jmplett@me.com>
2021-06-07 15:29:10 -0700

    Creation of data test

---
+++ SecretSanta/test/SecretSanta.Data.Tests/SecretSantaContextTests.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Data;
+
+namespace SecretSanta.Data.Tests
+{
+    [TestClass]
+    public class SecretSantaContextTests
+    {
+        [TestMethod]
+        public void test()
+        {
+        }
+    }
+}
